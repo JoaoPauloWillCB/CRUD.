9ea593d4a40296a3d08e361f4978f4e69f97bd5f
João Paulo Will Chagas Barbosa <joaopaulow010@gmail.com>
2024-07-24 23:46:55 -0300

    configs base e rotas iniciais

---
+++ app.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+use App\Controller\ControllerSite;
+use App\Controller\ControllerUser;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollector;
+use Slim\Routing\RouteCollectorProxy;
+
+require_once('vendor/autoload.php');
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+
+// SITE
+// $app->get('/', [ControllerSite::class, 'app']);
+$app->map(['GET', 'POST'], '/login', [ControllerSite::class, 'login']);
+$app->map(['GET', 'POST'], '/forgot-password', [ControllerSite::class, 'forgotPassword']);
+
+// USER
+$app->map(['GET', 'POST'], '/registration', [ControllerUser::class, 'registration']);
+//
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->run();
+
+// $app->group('/', function(RouteCollectorProxy $group){
+
+// });  
